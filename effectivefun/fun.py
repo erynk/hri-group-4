87f19f996be961d48686708a0a26b3a8c02571d7
Zabdiyel <ztan@mines.edu>
2021-11-05 13:24:27 -0600

    fixed jokes on the Nao

--- effectivefun/fun.py
+++ effectivefun/fun.py
@@ -20,19 +20,19 @@ def main(robotIP, PORT=9559):
 
     # The robot shares the daily schedule
     tts.say("^start(animations/Stand/Gestures/Hey_1) Good morning! I am your friend the NAO robot. ^stop(animations/Stand/Gestures/Hey_1) Here is your schedule for the day.")
-    tts.say("^start(animations/Stand/Gestures/Enthusiastic_4) First you t. The menu is eggs, bacon, pancakes, and orange juice. I wish I could eat that! ^stop(animations/Stand/Gestures/Enthusiastic_4)")
+    tts.say("^start(animations/Stand/Gestures/Enthusiastic_4) First you will have breakfast. The menu is eggs, bacon, pancakes, and orange juice. I wish I could eat that! ^stop(animations/Stand/Gestures/Enthusiastic_4)")
     tts.say("^start(animations/Stand/Gestures/You_1) Next it is time to take your medication. ^stop(animations/Stand/Gestures/You_1) ")
     tts.say("^start(animations/Stand/Gestures/Enthusiastic_5) After that Nurse Jenny will check in on you. She is nice! ^stop(animations/Stand/Gestures/Enthusiastic_5)")
-    tts.say("^start(animations/Stand/Gestures/Explain_8) Next is play time and you can choose to go to the game room, or the library. Why do ghosts like to visit the library? They go through the books very quickly! ^start(animations/Stand/Gestures/Explain_8)")
+    tts.say("^start(animations/Stand/Gestures/Explain_8) Next is play time and you can choose to go to the game room, or the library. I think you're cool, and that's nonfiction! ^start(animations/Stand/Gestures/Explain_8)")
     tts.say("After that is Lunch. The menu is Mac n cheese, apple sauce, and milk. Tasty!")
-    tts.say("^start(animations/Stand/Gestures/Enthusiastic_5) Next it will be time to do your schoolwork. What is a pirates favorite letter? Arrr! ^start(animations/Stand/Gestures/Enthusiastic_5)")
+    tts.say("^start(animations/Stand/Gestures/Enthusiastic_5) Next it will be time to do your schoolwork. I always angled for straight Ayess on my geometry exams! ^start(animations/Stand/Gestures/Enthusiastic_5)")
     tts.say("^start(animations/Stand/Gestures/You_4) After that you will get a visit from your brother. Yipee! ^stop(animations/Stand/Gestures/You_4)")
     tts.say("^start(animations/Stand/Gestures/Explain_10) Next it is time to get a CT scan of your lungs. ^stop(animations/Stand/Gestures/Explain_10)")
     tts.say("^start(animations/Stand/Gestures/Enthusiastic_4) After that Dr. Jones will come check up on you. She is the best! ^stop(animations/Stand/Gestures/Enthusiastic_4)")
     tts.say("Next it is dinner time. The menu is dinosaur chicken nuggets, sweet potato fries, ketchup, broccoli and kool aid. What day do chickens fear the most? Fry-days!")
     tts.say("^start(animations/Stand/Gestures/You_1) After that it is time to take your medication ^stop(animations/Stand/Gestures/You_1)")
     tts.say("Next Nurse Blain will check in on you. He is my favorite!")
-    tts.say("^start(animations/Stand/Gestures/Hey_6) Finally after that it is bedtime. Goodnight! Dont let the bed bugs bite! If they do, hit them with a shoe, until theyre black and blue! ^stop(animations/Stand/Gestures/Hey_6)")
+    tts.say("^start(animations/Stand/Gestures/Hey_6) Finally after that it is bedtime. Goodnight! Dont let the bed bugs bite! If they do, hit them with a shoe, until they're black and blue! ^stop(animations/Stand/Gestures/Hey_6)")
 
     tts.setParameter("pitchShift", 0)
 
