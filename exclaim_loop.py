bf743476e1ddb76ed1113be1fef8cd3667ffed2a
Austin Purdy <abpurdy@mines.edu>
2021-12-03 14:51:11 -0700

    add schedule comparison

--- exclaim_loop.py
+++ exclaim_loop.py
@@ -45,39 +45,43 @@ def main(robotIP, PORT=9559):
         #Get the new schedule
         previous_schedule = current_schedule
         current_schedule = read_csv()
+        
+        alternate = True
+    
+        animated = ALProxy("ALAnimatedSpeech", robotIP, PORT)
+        configuration = {"bodyLanguageMode":"disabled"} 
 
-        #If the current schedule differs from the old one, update the schedule
-        if current_schedule != previous_schedule:
-            #Check if this is the first time the schedule has been announced, if so, start with a greeting and say the whole schedule
+        animation_list = [["^start(animations/Stand/Gestures/Explain_2)", "^stop(animations/Stand/Gestures/Explain_2)"], ["^start(animations/Stand/Gestures/Explain_3)", "^stop(animations/Stand/Gestures/Explain_3)"]]
+    
+        config = 1
 
-            #Otherwise, say that the schedule has been changed and announce the change
-
+        if len(current_schedule) == len(previous_schedule):
+            for i in range(len(current_schedule)):
+                current_appointment = current_schedule[i]
+                previous_appointment = previous_schedule[i]
+                if(current_appointment.name != previous_appointment.name or current_appointment.minute != previous_appointment.minute or current_appointment.hour != previous_appointment.hour):
+                    animated.say(" Hello. Your schedule has changed. Instead of", configuration)
+                    animated.say(previous_appointment.name + "at" + previous_appointment.hour + " " + previous_appointment.minute, configuration)
+                    animated.say("you have" + current_appointment.name + "at" + current_appointment.hour + " " + current_appointment.minute, configuration)
+        else
+             The robot shares the daily schedule
+            animated.say("I am the NAO robot. New items have been uploaded to the schedule", configuration)
+            for item in schedule:
+                if config mod 3 == 0:
+                    animated.say(item, configuration)
+                elif alternate:
+                    animated.say(animation_list[0[0]] + item + animation_list[0[1]])
+                else:
+                    animated.say(animation_list[1[0]] + item + animation_list[1[1]])
+                alternate = not alternate
+                config = config + 1
+        
         #Then, compare the current time to the soonest event in the schedule
         current_time = time.localtime(time.time())
 
         #If it is time for an event, then announce the event 
 
-
-
-    alternate = True
-    
-    animated = ALProxy("ALAnimatedSpeech", robotIP, PORT)
-    configuration = {"bodyLanguageMode":"disabled"} 
-
-    animation_list = [["^start(animations/Stand/Gestures/Explain_2)", "^stop(animations/Stand/Gestures/Explain_2)"], ["^start(animations/Stand/Gestures/Explain_3)", "^stop(animations/Stand/Gestures/Explain_3)"]]
-    
-    config = 1
-    # The robot shares the daily schedule
-    animated.say(" Good morning! I am the NAO robot. Here is your schedule for the day. ", configuration)
-    for item in schedule:
-        if config mod 3 == 0:
-            animated.say(item, configuration)
-        elif alternate:
-            animated.say(animation_list[0[0]] + item + animation_list[0[1]])
-        else:
-            animated.say(animation_list[1[0]] + item + animation_list[1[1]])
-        alternate = not alternate
-        config = config + 1
+    #
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
